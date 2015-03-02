9c4011fc1e6235b9f018eebc76a5d1c31786aaa8
Morten Kirstein <info@wannabemedia.dk>
2015-03-02 16:26:34 +0100

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/**
+ * 1: DEPENDENCIES 
+ */
+
+ var  gulp = require('gulp'),                         // Gulp Core
+      sass = require('gulp-sass'),                    // Sass Compiler
+      sourcemaps = require('gulp-sourcemaps'),        // Generate Sourcemaps               
+      bourbon = require('node-bourbon'),              // Sass Mixin
+      rename = require('gulp-rename'),                // rename files
+      notify = require('gulp-notify'),                // Send notifications to OSX
+      concat = require('gulp-concat'),                // Concatting Files
+      plumber = require('gulp-plumber'),              // Disable Interrupts
+      minifycss = require('gulp-minify-css'),         // Minifying css
+      browserSync = require('browser-sync'),          // Sync stylesheets and devices 
+      autoPrefixer = require('gulp-autoprefixer');    // Prefix stylesheet
+
+
+
+/**
+ * 2: TARGETS 
+ */
+
+ var target = {
+    scss_src : 'app/sass/**/*.scss',
+    css_dest : 'app/css/'
+ };
+
+
+ 
+ /**
+ * 3: SASS TASK
+ */
+
+ bourbon.includePaths;
+
+ var SassOptions = {
+    sourcemap: true,
+    style: "compact",
+    includePaths: require('node-bourbon').includePaths
+};
+
+gulp.task('sass', function() {
+    gulp.src(target.scss_src)                       //  Get the files
+      
+      .pipe(plumber())                              //  Make sure gulp continues on errors
+      .pipe(sourcemaps.init())
+      .pipe(sass(SassOptions))                      //  Compile all sass
+      // .pipe(minifycss())                         //  Minifycss
+      .pipe(sourcemaps.write())                     //  Add Source File                       
+      .pipe(gulp.dest(target.css_dest))             //  Where to put files
+      .pipe(notify({message: 'SCSS Processed'}));   //  Notify on process done
+});
+
+
+ /**
+ * 4: BROWSER INIT
+ */
+
+gulp.task('browser-sync', function() {
+    browserSync.init(['app/AuroraESite/StoreAssets/css/*.css'],{
+        proxy: "tm:8000"  
+
+    });
+});
+
+
+ /**
+ * 5: WATCHES
+ */
+
+ gulp.task('default', function() {
+     gulp.run('sass', 'browser-sync');
+     gulp.watch('app/sass/**/*.scss', function(){
+        gulp.run('sass');
+     });
+ });
+
+
+
